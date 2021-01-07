1b41e5e44f61535e68e17d220e03d86d57df0b6a
kyomira <76930830+kyomira@users.noreply.github.com>
2021-01-07 17:07:15 +0800

    update liuyao2.py

--- python/liuyao.py
+++ python/liuyao.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python3
 
+import sys
+
+def launch():
+    print("what's your doubt?")
+    selection = input("1 for yes/no, 2 for liuyao: ")
+    print("selection is:" + selection)
+    if selection == "1":
+        print("yes/no")
+    elif selection == "2":
+        liuyao()
+    else:
+        print("ohlala")
+        sys.exit()
+
+def liuyao():
+    print("liuyao")
+
+
 if __name__ == "__main__":
-    print("hello, world")
-    print("copy that")
+    launch()
