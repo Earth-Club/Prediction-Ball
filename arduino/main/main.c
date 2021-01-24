4425398a2cf3447dbedd7c785bcbb082dff95668
Robin Zhong <fbzhong@gmail.com>
2021-01-24 18:57:20 +0800

    Add trigram.h & main.c, and move main() to main.c from trigram.c.

---
+++ arduino/main/main.c
@@ -0,0 +1,127 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include "trigram.h"
+
+int main(int argc, char **argv) {
+  srand(time(0));
+
+  RAWTRIGRAM raw = rolldice();
+  // RAWTRIGRAM raw = 1952;
+
+  printf("======================\n");
+
+  rawtrigram_print(raw);
+  TRIGRAM trigram = trigram_new(raw);
+  trigram_print(trigram, "TRIGRAM ");
+  trigram_print_eight_gong(trigram);
+  trigram_print_ben_gong_trigram(trigram);
+
+  printf("======================\n");
+
+  RAWTRIGRAM alterraw = rawtrigram_alter(raw);
+  rawtrigram_print(alterraw);
+  TRIGRAM altered_trigram = trigram_new(alterraw);
+  trigram_print(altered_trigram, "TRIGALTER ");
+  trigram_print_eight_gong(altered_trigram);
+  trigram_print_ben_gong_trigram(altered_trigram);
+
+  printf("======================\n");
+
+  // RAWTRIGRAM raw_altered = rawtrigram_alter(raw);
+  // rawtrigram_print(raw_altered);
+  // TRIGRAM altered_trigram = trigram_new(raw_altered);
+  // trigram_print(altered_trigram, "TRIGRAM_");
+  printf("\n");
+
+  time_t now = time(NULL);
+  // printf("%s", ctime(&now));
+
+  struct tm *nowtm = localtime(&now);
+
+  Date solar;
+  solar.year = nowtm->tm_year + 1900;
+  solar.month = nowtm->tm_mon + 1;
+  solar.day = nowtm->tm_mday;
+  solar.hour = nowtm->tm_hour;
+
+  Date lunar = {0}, gan = {0}, zhi = {0}, lunar2 = {0}, gan2 = {0}, zhi2 = {0};
+  int jieAlert = 0;
+
+  Solar2Lunar(&solar, &lunar, &gan, &zhi, &lunar2, &gan2, &zhi2, &jieAlert);
+
+  printf("%s%d%s%2d%s%2d%s%2d%s%s%s\n", "阳历    ：", solar.year, "年",
+         solar.month, "月", solar.day, "日", solar.hour, "时　", "星期",
+         weekdayUTF8[solar.weekday]);
+  printf("%s%d%s%s%2d%s%2d%s%s%s%s%s\n", "阴历    ：", lunar.year, "年",
+         (lunar.leap ? "闰" : ""), lunar.month, "月", lunar.day, "日",
+         ZhiUTF8[zhi.hour], "时　", "生肖属", ShengXiaoUTF8_hans[zhi.year]);
+  printf("%s%s%s%s%s%s%s%s%s%s%s%s%s\n", "干支    ：", GanUTF8[gan.year],
+         ZhiUTF8[zhi.year], "年　", GanUTF8[gan.month], ZhiUTF8[zhi.month],
+         "月　", GanUTF8[gan.day], ZhiUTF8[zhi.day], "日　", GanUTF8[gan.hour],
+         ZhiUTF8[zhi.hour], "时　");
+  printf("%s%s%s%s%s%s%s%s%s%s%s%s%s\n", "四柱神算：", GanUTF8[gan2.year],
+         ZhiUTF8[zhi2.year], "年　", GanUTF8[gan2.month], ZhiUTF8[zhi2.month],
+         "月　", GanUTF8[gan2.day], ZhiUTF8[zhi2.day], "日　",
+         GanUTF8[gan2.hour], ZhiUTF8[zhi2.hour],
+         "时　(用四柱神算推算之时辰八字)");
+  if (jieAlert) {
+    printf("* %s, %s\n", "是日为节", "月柱可能要修改");
+    if (lunar2.month == 1) printf("* %s\n", "年柱亦可能要修改");
+    printf("* %s\n", "请查有节气时间之万年历");
+  }
+  printf("\n");
+
+  // six god.
+  int six_god_of_first_yao = sixagenary_day_to_six_god(zhi.day);
+
+  // Trigram.
+  int orig_shi_yao_idx = 0;
+  int orig_ying_yao_idx = 0;
+  int orig_trigram_eight_gong = 0;
+  trigram_get_ben_gong_trigram(trigram, NULL, &orig_trigram_eight_gong,
+                               &orig_shi_yao_idx, &orig_ying_yao_idx);
+  int orig_trigram_five_element =
+      eight_gong_to_five_element(orig_trigram_eight_gong);
+
+  // Altered Trigram.
+  int altered_shi_yao_idx = 0;
+  int altered_ying_yao_idx = 0;
+  int altered_trigram_eight_gong = 0;
+  trigram_get_ben_gong_trigram(altered_trigram, NULL,
+                               &altered_trigram_eight_gong,
+                               &altered_shi_yao_idx, &altered_ying_yao_idx);
+  int altered_trigram_five_element =
+      eight_gong_to_five_element(altered_trigram_eight_gong);
+
+  printf("      六神        本      卦              变      卦\n");
+  printf("                  %s(%2d)            %s(%2d)\n",
+         TRIGRAM_NAMES[(int)trigram], (int)trigram,
+         TRIGRAM_NAMES[(int)altered_trigram], (int)altered_trigram);
+
+  // alter trigram.
+
+  for (int i = 5; i >= 0; i--) {
+    // name of yao.
+    char *yao_name = YAO_NAMES[i];
+
+    // Six God.
+    char *six_god = SIX_GODS[(six_god_of_first_yao + i) % 6];
+
+    char trigram_str[64] = {0};
+    format_trigram(trigram_str, trigram, i, orig_shi_yao_idx, orig_ying_yao_idx,
+                   orig_trigram_five_element);
+
+    char altered_trigram_str[64] = {0};
+    format_trigram(altered_trigram_str, altered_trigram, i, altered_shi_yao_idx,
+                   altered_ying_yao_idx, altered_trigram_five_element);
+
+    printf("%s爻: %s     %s     %s\n", yao_name, six_god, trigram_str,
+           altered_trigram_str);
+  }
+
+  return 0;
+}
--- arduino/main/trigram.c
+++ arduino/main/trigram.c
@@ -1,58 +1,16 @@
+#include "trigram.h"
+
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
-#include "lunar-date.h"
-
 #define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c"
 #define BYTE_TO_BINARY(byte)                                \
   (byte & 0x20 ? '1' : '0'), (byte & 0x10 ? '1' : '0'),     \
       (byte & 0x08 ? '1' : '0'), (byte & 0x04 ? '1' : '0'), \
       (byte & 0x02 ? '1' : '0'), (byte & 0x01 ? '1' : '0')
-
-#ifdef LUNAR_DATE_H
-#define EARTHLY_BRANCHES ZhiUTF8
-#else
-static char *EARTHLY_BRANCHES[] = {"子", "丑", "寅", "卯", "辰", "巳",
-                                   "午", "未", "申", "酉", "戌", "亥"};
-#endif
-
-static char *YAO_NAMES[] = {"初", "二", "三", "四", "五", "上"};
-
-static char *TRIGRAM_NAMES[] = {
-    "坤为地",   "山地剥",   "水地比", "风地观",   "雷地豫",   "火地晋",
-    "泽地萃",   "天地否",   "地山谦", "艮为山",   "水山蹇",   "风山渐",
-    "雷山小过", "火山旅",   "泽山咸", "天山遁",   "地水师",   "山水蒙",
-    "坎为水",   "风水涣",   "雷水解", "火水未济", "泽水困",   "天水讼",
-    "地风升",   "山风蛊",   "水风井", "巽为风",   "雷风恒",   "火风鼎",
-    "泽风大过", "天风姤",   "地雷复", "山雷颐",   "水雷屯",   "风雷益",
-    "震为雷",   "火雷噬嗑", "泽雷随", "天雷无妄", "地火明夷", "山火贲",
-    "水火既济", "风火家人", "雷火丰", "离为火",   "泽火革",   "天火同人",
-    "地泽临",   "山泽损",   "水泽节", "风泽中孚", "雷泽归妹", "火泽睽",
-    "兑为泽",   "天泽履",   "地天泰", "山天大畜", "水天需",   "风天小畜",
-    "雷天大壮", "火天大有", "泽天夬", "乾为天"};
-
-static char *EIGHT_GONGS[] = {"坤", "艮", "坎", "巽", "震", "离", "兑", "乾"};
-
-static char *FIVE_ELEMENTS[] = {"金", "木", "水", "火", "土"};
-// 木生火，火生土，土生金，金生水，水生木
-static int FIVE_ELEMENTS_PROMITING[] = {2, 3, 1, 4, 0};
-// 木剋土，土剋水，水剋火，火剋金，金剋木
-static int FIVE_ELEMENTS_REGULATING[] = {1, 4, 3, 0, 2};
-
-// 父母生兄弟，兄弟生子孫，子孫生妻財，妻財生官鬼，官鬼生父母
-// 父母剋子孫，子孫剋官鬼，官鬼剋兄弟，兄弟剋妻財，妻財剋父母
-static char *SIX_RELATIVES[] = {"父母", "兄弟", "子孙", "妻财", "官鬼"};
-
-// 原来天干是用Yin/Yang的五行来表示，地支是用12生肖来表示。那这个每个爻对应的六神，是根据干支纪日来对应的。比如甲乙（日），Yang
-// Wood & Yin
-// Wood，Wood属木，木为青龙，则初爻为青龙，然后按“一青龍、二朱雀、三勾陳、四「騰蛇」、五白虎，六玄武”这样的顺序给二爻、三爻、四爻、五爻、上爻对应上。
-// 有一点不太一样的就是“戊 Yang Earth”和“己 Yin
-// Earth”，都是土，但戊是勾陈，己是腾蛇属火。 from
-// https://en.wikipedia.org/wiki/Sexagenary_cycle
-static char *SIX_GODS[] = {"青龙", "朱雀", "勾陈", "螣蛇", "白虎", "玄武"};
 
 int eight_gong_to_five_element(int idx) {
   switch (idx) {
@@ -129,20 +87,6 @@ int sixagenary_day_to_six_god(int day) {
     return (day / 2) + 1;
   }
 }
-
-// Library of Trigram.
-// 1. roll 3 dices at the same time to generate 6 random numbers in [0,3).
-// 2. convert those 6 numbers by applying f(x)=x%2 function to a group of yao,
-// which is a trigram (0bABCDEF).
-// 3. lookup the Five Element names by Earthly Branch of Yao (inner or outer).
-// 4. lookup the Six Relative names by Eight Gong of Trigram.
-// 5. lookup the 世爻/应爻 by Five Element of Trigram.
-
-// TRIGRAM [初爻, 二爻，三爻，四爻，五爻，六爻]
-#define TRIGRAM char
-
-// RAWTRIGRAM [初爻, 二爻，三爻，四爻，五爻，六爻]
-#define RAWTRIGRAM short
 
 RAWTRIGRAM rolldice() {
   printf("rolldice: ");
@@ -490,7 +434,7 @@ int format_trigram(char *buf, TRIGRAM trigram, int i, int shi_yao_idx,
 #endif
 }
 
-int main() {
+void demo() {
   srand(time(0));
 
   RAWTRIGRAM raw = rolldice();
@@ -606,6 +550,4 @@ int main() {
     printf("%s爻: %s     %s     %s\n", yao_name, six_god, trigram_str,
            altered_trigram_str);
   }
-
-  return 0;
 }
